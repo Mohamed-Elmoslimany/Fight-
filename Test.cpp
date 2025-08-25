a42571fa4972badeed120403c90b1007eede3271
Mohamed Elmoslimany <elmoslimanym@gmail.com>
2025-08-26 00:03:26 +0300

    first commit

---
+++ Test.cpp
@@ -0,0 +1,15 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    cout << "Hello world!" << endl;
+    string s;
+    cin >> s;
+    cout << "You entered: " << s << endl;
+    return 0;
+}
